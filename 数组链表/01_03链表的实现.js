4e7573decc518f20eb44d4e3b3c69ef626131bef
Yuan Wang <yuanwang612@gmail.com>
2024-02-06 20:06:01 -0800

    week 1 ; ` exit ;

--- 数组链表/01_03链表的实现.js
+++ 数组链表/01_03链表的实现.js
@@ -1,5 +1,16 @@
 // 双链表节点
+class LinkedListNode {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
 // ******增*******
 // 在链表头部添加一个元素
 // 在链表尾部添加一个元素
@@ -11,5 +22,9 @@
 // 删掉特定位置的元素
 
 // ******查*******
+// 返回链表头部的元素
+// 返回链表尾部的元素
+// 返回链表特定位置的元素
 
 // ******改*******
+// 更改特定位置的元素
---
+++ 数组链表/复习App Academy/debug.js
@@ -0,0 +1,8 @@
+function countdown(num) {
+  while (num !== 0) {
+    console.log(num);
+    num--;
+  }
+}
+
+countdown(5);
