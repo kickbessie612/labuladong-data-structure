71e9f077f3193bf4f19d5815e92a50537577aac8
Yuan Wang <yuanwang612@gmail.com>
2024-05-23 22:24:22 -0700

    add move zeros

---
+++ 数组链表/复习App Academy/moveZeros.js
@@ -0,0 +1,14 @@
+// Given an array nums, write a function to move all 0's to the end of it while
+// maintaining the relative order of the non-zero elements.
+
+// Input: [0,1,0,3,12] Output: [1,3,12,0,0]
+
+// * You must do this in-place without making a copy of the array.
+// * Minimize the total number of operations.
+
+// plan
+
+// 1. Set a pointer to the last element in the array.
+// 2. Walk through each element of the array.
+// 3. If the element is zero, swap its position with the element at the pointer,
+//    then decrement the pointer.
