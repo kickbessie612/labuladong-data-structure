97873b6b30ee1582c5e5c629c2d37657846a89d2
Yuan Wang <yuanwang612@gmail.com>
2024-05-24 21:31:32 -0700

    carry out plans

--- 数组链表/复习App Academy/moveZeros.js
+++ 数组链表/复习App Academy/moveZeros.js
@@ -12,3 +12,10 @@
 // 2. Walk through each element of the array.
 // 3. If the element is zero, swap its position with the element at the pointer,
 //    then decrement the pointer.
+
+function moveZeroes(nums) {
+  // 1. Set a pointer to the last element in the array.
+  // 2. Walk through each element of the array.
+  // 3. If the element is zero, swap its position with the element at the pointer,
+  // then decrement the pointer.
+}
