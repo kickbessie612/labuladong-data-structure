94c5e0ac9952edd4504cb68f4cbd8d08aaff8976
Yuan Wang <yuanwang612@gmail.com>
2024-06-04 11:06:01 -0700

    review

--- 数组链表/复习App Academy/moveZeros.js
+++ 数组链表/复习App Academy/moveZeros.js
@@ -17,7 +17,7 @@ function moveZeroes(nums) {
   // 1. Set a pointer to the last element in the array.
   let last = nums.length - 1;
 
-  // 2. Walk through each element of the array. using loop to reieriate
+  // 2. Walk through each element of the array. using loop to reieriate teach element6
 
   // 3. If the element is zero, swap its position with the element at the pointer,
   // then decrement the pointer.
