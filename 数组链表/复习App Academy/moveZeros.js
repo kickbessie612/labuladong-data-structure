395eac3c8b0d4f37bd16b4635ae100e4066a96e3
Yuan Wang <yuanwang612@gmail.com>
2024-06-12 23:10:55 -0700

    complete move zeroes

--- 数组链表/复习App Academy/moveZeros.js
+++ 数组链表/复习App Academy/moveZeros.js
@@ -17,12 +17,15 @@ function moveZeroes(nums) {
   // 1. Set a pointer to the last element in the array.
   let last = nums.length - 1;
 
-  // 2. Walk through each element of the array. using loop to reieriate teach element6
-
-  // 3. If the element is zero, swap its position with the element at the pointer,
-  // then decrement the pointer.
-  if (nums[0] === 1) {
-    return;
-    // maybe using sh
+  // 2. Walk through each element of the array.
+  for (let i = 0; i < last; i++) {
+    // 3. If the element is zero,
+    if (nums[i] == 0) {
+      // swap its position with the element at the pointer,
+      [nums[i], nums[last]] = [nums[last], nums[i]];
+      // then decrement the pointer.
+      last--;
+    }
   }
+  return nums;
 }
