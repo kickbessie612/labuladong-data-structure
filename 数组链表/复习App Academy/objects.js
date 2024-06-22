d8f2b2e93c54eba4122c44969281f0566dc63d9e
Yuan Wang <yuanwang612@gmail.com>
2024-06-21 21:49:48 -0700

    review object

--- 数组链表/复习App Academy/objects.js
+++ 数组链表/复习App Academy/objects.js
@@ -6,11 +6,23 @@ car.seat = 2;
 
 console.log(car);
 
-let myDog = {
-  name: 'Fido',
-  type: 'Doge',
-  age: 2,
-  favoriteToys: ['bone', 'ball']
-};
+// let myDog = {
+//   name: 'Fido',
+//   type: 'Doge',
+//   age: 2,
+//   favoriteToys: ['bone', 'ball']
+// };
 
-console.log(myDog);
+// console.log(myDog);
+
+let myDog = {};
+myDog.name = 'Fido';
+
+let myKey = 'name';
+console.log(myDog); // prints `{name: "Fido"}`
+console.log(myDog[myKey]); // prints `Fido`
+
+//  myDog[myKey] = myDog['name']
+
+// myDog.myKey = myDog
+console.log(myDog.myKey); // prints: undefined
