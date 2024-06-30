c17ba7af8ecb64948257bbf2588186f83b8bc0e8
Yuan Wang <yuanwang612@gmail.com>
2024-06-29 21:24:25 -0700

    spread objects

--- 数组链表/复习App Academy/spreadOperator.js
+++ 数组链表/复习App Academy/spreadOperator.js
@@ -1,4 +1,18 @@
+// spread array
+
 let numArray = [1, 2, 3];
 let moreNums = [...numArray, 4, 5, 6];
 
 console.log(moreNums);
+
+// spread objects
+
+let colors = { red: 'scarlet', blue: 'aquamarine' };
+let newColors = { ...colors };
+
+console.log(newColors);
+
+let colors2 = { green: 'forest', yellow: 'sunflower' };
+let moreColors = { ...colors, ...colors2 };
+
+console.log(moreColors);
