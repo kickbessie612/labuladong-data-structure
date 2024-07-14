fcef0644ba51f2c754ac86d6cc0138be0825ba05
Yuan Wang <yuanwang612@gmail.com>
2024-07-13 22:00:37 -0700

    swapping array

--- 数组链表/复习App Academy/destructuring.js
+++ 数组链表/复习App Academy/destructuring.js
@@ -17,3 +17,14 @@ let animal1, animal2;
 
 console.log(animal1); //=> "tiger"
 console.log(animal2); //=> "hippo"
+
+// Swapping
+
+let num1 = 17;
+let num2 = 3;
+
+// this syntax will swap the values of the two variables
+[num1, num2] = [num2, num1];
+
+console.log(num1); // 3
+console.log(num2); // 17
