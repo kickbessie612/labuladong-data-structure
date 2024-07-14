e3cb78ddf223f8266168dd079984871b04cc645d
Yuan Wang <yuanwang612@gmail.com>
2024-07-13 21:58:55 -0700

    destrucuring array

---
+++ 数组链表/复习App Academy/destructuring.js
@@ -0,0 +1,9 @@
+// Destructure an array to reference specific elements
+
+let numArray = [10, 20];
+
+// here we are "unpacking" the array values into two separate variables
+let [firstEl, secondEl] = numArray;
+
+console.log(firstEl); //=> 10
+console.log(secondEl); //=> 20
