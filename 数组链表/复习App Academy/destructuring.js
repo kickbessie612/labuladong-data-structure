1064c6a1163f427051096da79fe3498510d88e4e
Yuan Wang <yuanwang612@gmail.com>
2024-07-13 21:59:51 -0700

    declare variables

--- 数组链表/复习App Academy/destructuring.js
+++ 数组链表/复习App Academy/destructuring.js
@@ -7,3 +7,13 @@ let [firstEl, secondEl] = numArray;
 
 console.log(firstEl); //=> 10
 console.log(secondEl); //=> 20
+
+let animalArray = ['tiger', 'hippo'];
+
+let animal1, animal2;
+
+// here we are "unpacking" the array values into two separate variables
+[animal1, animal2] = animalArray;
+
+console.log(animal1); //=> "tiger"
+console.log(animal2); //=> "hippo"
